71e80918314db0649be0b1cf3a2b3bd683b90d2d
Ashish Mitra <161473324+ashish-mitra@users.noreply.github.com>
2024-12-03 23:24:09 +0530

    a

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,37 @@
+const tl = gsap.timeline()
+
+
+tl.to(".animate-block-1",{
+    // opacity:0,
+    delay:0.8,
+    x : 2000,
+    duration:8,
+    ease: "power3.out"
+}, 0)
+
+ .to(".animate-block-2",{
+    // opacity:0,
+    delay:0.8,
+    x : -2000,
+    duration:8,
+    ease: "power3.out"
+}, 0);
+
+
+
+tl.from(".hero-section",{
+    delay:1.6,
+    opacity:0,
+    y : 200,
+    duration:1,
+    ease: "power2.out"
+}, 1);
+
 gsap.from(".card1", {
     opacity:0,
     x : -1200,
     y : 200,
-    ease: "power4.out",
+    ease: "power3.out",
     scrollTrigger:{
         trigger:".card1",
         scroller :"body", 
@@ -18,7 +47,7 @@ gsap.from(".card2", {
     opacity:0,
     x : 1200,
     y : 200,
-    ease: "power4.out",
+    ease: "power3.out",
     scrollTrigger:{
         trigger:".card2",
         scroller :"body", 
@@ -33,7 +62,7 @@ gsap.from(".card2", {
 gsap.from("#red", {
     opacity:0,
     scale : 0.4,
-    ease: "power4.out",
+    ease: "power2.out",
     scrollTrigger:{
         trigger:"#red",
         scroller :"body", 
